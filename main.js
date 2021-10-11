a8bf2899f022ebb66b8240a752933ca328a8ba85
NicolaL95 <nicola.lilla@gmail.com>
2021-10-11 15:46:07 +0200

    Fix

--- main.js
+++ main.js
@@ -1,10 +1,11 @@
 
 
 let num_km = prompt("Benvenuto nel servizio di ticketing automatico di Boolitalia. Inserisca il numero di KM che vuole percorrere:").match(/\d+/g);
+/* .match utilizzato nell'eventualità che l'utente inseriesca n.km + l'unità di misura km */
 
 let user_age = prompt("E' possibile usufruire di una scontistica riservata ai minorenni(20%) e agli over 65(40%). Inserisca la sua età:");
 
-let price_km = (num_km * 0.21)
+let price_km = (num_km * 0.21);
 
 let final_price = price_km;
 
